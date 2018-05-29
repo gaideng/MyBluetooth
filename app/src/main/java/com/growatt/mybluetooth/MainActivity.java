c646d2ac8b5e10e64fd3d87fd3d63ff87548b272
gaideng <Liuying050615>
2018-05-29 10:55:07 +0800

    Init commit

---
+++ app/src/main/java/com/growatt/mybluetooth/MainActivity.java
@@ -0,0 +1,56 @@
+package com.growatt.mybluetooth;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.inuker.bluetooth.library.BluetoothClient;
+import com.inuker.bluetooth.library.connect.listener.BluetoothStateListener;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button btnStart;
+    private Button btnStop;
+    private BluetoothClient mClient;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnStart = findViewById(R.id.btnStart);
+        btnStop = findViewById(R.id.btnStop);
+        btnStart.setOnClickListener(this);
+        btnStop.setOnClickListener(this);
+        mClient = new BluetoothClient(this);
+        //蓝牙状态监听
+        mClient.registerBluetoothStateListener(mBluetoothStateListener);
+    }
+    private final BluetoothStateListener mBluetoothStateListener = new BluetoothStateListener() {
+        @Override
+        public void onBluetoothStateChanged(boolean openOrClosed) {
+                if (!openOrClosed){
+                    //蓝牙未开启，提示开启蓝牙
+                    mClient.openBluetooth();
+                }else {
+                    //蓝牙已开启
+                }
+        }
+
+    };
+    @Override
+    public void onClick(View view) {
+        if (view == btnStart){
+            //判断蓝牙是否打开
+            mClient.isBluetoothOpened();
+        }
+        if (view == btnStop){
+            mClient.stopSearch();
+            mClient.closeBluetooth();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mClient.unregisterBluetoothStateListener(mBluetoothStateListener);
+    }
+}
